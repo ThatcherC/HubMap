9e51a6888c4364e596943dbe6769ae3b43cadf52
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-03-29 17:21:17 -0400

    Can now get and parse distance matrices from Google.

--- mapper.js
+++ mapper.js
@@ -1,4 +1,4 @@
-var http = require("http");
+var https = require("https");
 var polyline = require('polyline');
 var configs = require('./config');
 
@@ -9,8 +9,28 @@ stations["Stata"] = [42.361989, -71.092061];
 stations["Ames"] = [42.362536, -71.088188];
 stations["Kendall"] = [42.362482, -71.085124];
 
+var nexthouse = [42.355005, -71.101438];
+
 var base_url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
-base_url+="key="+configs.api_key;
-console.log(base_url);
+base_url+="key="+configs.api_key+"&";
 
 //Precomputed distances request:
+//Next to Vassar and Mass Ave, walking
+var origin = "enc:"+polyline.encode([nexthouse])+":";
+var destinations = "enc:"+polyline.encode([stations["Vassar"],stations["Mass Ave"]])+":";
+var req_url = base_url;
+req_url += "origins="+origin+"&";
+req_url += "destinations="+destinations+"&";
+req_url += "mode=walking"
+
+https.get(req_url,function(res){
+  var data = "";
+  var matrix = {};
+  res.on('data', function(chunk){
+    data+=chunk;
+  });
+  res.on('end',function(){
+    matrix = JSON.parse(data).rows;
+    console.log(matrix);
+  });
+});
